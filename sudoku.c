a8648c771a1931ba94b3c4a527ca4f0db7982344
John Salman <31483839+john-salman@users.noreply.github.com>
2018-03-31 12:23:00 -0700

    Update sudoku.c
    Commented the code for clarity

--- sudoku.c
+++ sudoku.c
@@ -5,25 +5,25 @@
 #include <string.h>
 
 int main() {
-  char sudokuBoard[9][9];
-  int row = 0;
-  int column = 0;
-  int i = 0;
+  char sudokuBoard[9][9]; // 2D array, we could make this an int if we wanted to
+  int row = 0;	  // row  count
+  int column = 0; // column count
+  int i = 0;	  // indexer	
   const size_t line_size = 300;
-  char* line = malloc(line_size);
-  while (fgets(line, line_size, stdin) != NULL)  {
-    while (line[i] != '\n' && line[i] != '\0') {
-      if (line[i] != ' ') {
+  char* line = malloc(line_size); // basically a buffer
+  while (fgets(line, line_size, stdin) != NULL)  { // while the line is not null in the item in stdin
+    while (line[i] != '\n' && line[i] != '\0') { // while theres not a new line or terminator
+      if (line[i] != ' ') { 
 	printf("Storing value: %c,in row: %d, column: %d\n", line[i], row, column); 
-	sudokuBoard[row][column] = line[i];
+	sudokuBoard[row][column] = line[i]; // stores items cloumn by column
 	column++;
       }
       i++;
     }
-    if (line[0] != '\n') {
-      row++;
+    if (line[0] != '\n') { // because row should never be reset we want to keep an accurate row count and disregard
+      row++;               // i think that this could prob get placed on line 22 tbh
     }
-    column = 0;
+    column = 0; // reset column
     printf("\n");
     i = 0;
   }
